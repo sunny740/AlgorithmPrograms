c707e8be5bdb812193b7a6982084a0fdc4bdbb94
sunny740 <sunnysejwarvitm005@gmail.com>
2022-05-08 18:34:32 +0530

    [Sunny] Add : Binary Search Implemented

---
+++ AlgorithmPrograms/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BinarySearch
+    {
+        public void binary(string[] inputArray, string key)
+        {
+            int Start = 0;
+            int end = inputArray.Length - 1;
+            while (Start <= end)
+            {
+                int mid = (Start + end) / 2;
+                if (key == inputArray[mid])
+                {
+                    Console.WriteLine("The word is at the middle position {0}", mid);
+                    break;
+                }
+                if (key.CompareTo(inputArray[mid]) > 0)
+                {
+                    Start = mid - 1;
+                    Console.WriteLine("The word is at position {0}", Start);
+                    break;
+                }
+                else
+                {
+                    end = mid + 1;
+                    Console.WriteLine("The word is at position {0}", end);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AlgorithmPrograms;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome To The Program");
+        bool check = true;
+        const string path = @"E:\BridgeLabzProject\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\ProgramsData.txt";
+        BinarySearch binary = new BinarySearch();
+        const string path1 = @"E:\BridgeLabzProject\AlgorithmPrograms\AlgorithmPrograms\AlgorithmPrograms\intvalues.txt";
+        
+        Console.WriteLine("1.Binary Search\n");
+        while (check)
+        {
+            Console.WriteLine("\nTake an Option To Execute");
+            int opt = Convert.ToInt32(Console.ReadLine());
+            switch (opt)
+            {
+                case 1:
+                    string words = File.ReadAllText(path);
+                    string[] input_Array = words.Split(',');
+                    Console.WriteLine("Enter The Value");
+                    string word = Console.ReadLine();
+                    binary.binary(input_Array, word);
+                    break;
+                
+                case 2:
+                    Console.WriteLine("Enter a Valid Choice");
+                    break;
+                default: break;
+            }
+        }
+    }
+}
